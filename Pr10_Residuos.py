69b7e5e60b5ccc9c9a2ee55c92b3bb406112dd42
AlexisMoraMeza <amorameza8@gmail.com>
2025-11-19 09:25:38 -0600

    Delete Pr10_Residuos.py

--- Pr10_Residuos.py
+++
@@ -1,7 +0,0 @@
-#3º B Programacion Matutino
-#Autor: Alexis Mora Meza
-#Objetivo: saludar al usuario y mostrar su edad el proximo año
-num1 = int(input("teclea el primer numero: "))
-num2 = int(input("teclea el segundo numero: "))
-residuo = num1 % num2
-print("El residuo de la división es:", residuo)
