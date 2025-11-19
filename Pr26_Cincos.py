dc795ee7f997651607ceed0b7f42cf5130c32d91
AlexisMoraMeza <amorameza8@gmail.com>
2025-11-19 09:28:17 -0600

    Delete Pr26_Cincos.py

--- Pr26_Cincos.py
+++
@@ -1,8 +0,0 @@
-#3º B Programacion Matutino
-#Autor: Alexis Mora Meza
-#Objetivo: saludar al usuario y most
-sum = 0
-for j in range(10, 151, 5):
-    print(j)
-    sum=sum+j
-print("La suma de todos los numeros son:",sum)
