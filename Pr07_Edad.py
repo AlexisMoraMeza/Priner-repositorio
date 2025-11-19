6b803c0716bea69781bd4f468fc21b39827b5e7b
AlexisMoraMeza <amorameza8@gmail.com>
2025-11-19 09:25:12 -0600

    Delete Pr07_Edad.py

--- Pr07_Edad.py
+++
@@ -1,8 +0,0 @@
-#3º B Programacion Matutino
-#Autor: Alexis Mora Meza
-#Objetivo: saludar al usuario y mostrar su edad el proximo año
-AnioA = int(input("Teclea el año actual: "))
-AnioN = int(input("Teclea la año en que naciste: "))
-
-Edad= (AnioA-AnioN)
-print("tu edad es: ", Edad)
