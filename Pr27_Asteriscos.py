5e6f899d6659f0ea2b04d9ac07e2f2717a05038a
AlexisMoraMeza <amorameza8@gmail.com>
2025-11-19 09:28:26 -0600

    Delete Pr27_Asteriscos.py

--- Pr27_Asteriscos.py
+++
@@ -1,7 +0,0 @@
-#3º B Programacion Matutino
-#Autor: Alexis Mora Meza
-#Objetivo: saludar al usuario y most
-N = int(input("Teclea el numero de filas: "))
-
-for i in range(1, N + 1):
-    print("*" * i)
