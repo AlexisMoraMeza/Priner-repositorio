440d7f39d29c6688b8246ea14230cad2cb94a78e
AlexisMoraMeza <amorameza8@gmail.com>
2025-11-19 09:26:02 -0600

    Delete Pr28_Factorial.py

--- Pr28_Factorial.py
+++
@@ -1,7 +0,0 @@
-#3º B Programacion Matutino
-#Autor: Alexis Mora Meza
-#Objetivo: saludar al usuario y most
-N = int(print("Teclea un numero para saber su factorial:"))
-fact=1
-for i in range(1, N + 1):
-    fact *=i
