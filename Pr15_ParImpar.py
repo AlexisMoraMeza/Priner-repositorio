587ef7a30a17c4eedbb22afab4123914efd6a3d8
AlexisMoraMeza <amorameza8@gmail.com>
2025-11-19 09:26:43 -0600

    Delete Pr15_ParImpar.py

--- Pr15_ParImpar.py
+++
@@ -1,9 +0,0 @@
-#3º B Programacion Matutino
-#Autor: Alexis Mora Meza
-#Objetivo: saludar al usuario y mostrar su edad el proximo año
-num = int(input("Teclea un numero: "))
-
-if num % 2 == 0:
-    print("es par.")
-else:
-    print("es impar.")
