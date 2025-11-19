a3706c8361301b0bbbf8a98805f82ca92475cc68
AlexisMoraMeza <amorameza8@gmail.com>
2025-11-19 09:25:03 -0600

    Delete Pr06_AreaTriangulo.py

--- Pr06_AreaTriangulo.py
+++
@@ -1,8 +0,0 @@
-#3º B Programacion Matutino
-#Autor: Alexis Mora Meza
-#Objetivo: saludar al usuario y mostrar su edad el proximo año
-B = int(input("Teclea la base del triángulo: "))
-A = int(input("Teclea la altura del triángulo: "))
-
-Area= (B*A)/2
-print("el area del triángulo es: ", Area)
